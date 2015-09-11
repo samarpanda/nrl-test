94d9f74ee365502b1a5d695d9b9ef7d5fdde076e
Samar Panda <samar.panda84@gmail.com>
2015-09-11 21:54:31 +0530

    removed dead code

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,4 @@ import splitSentence from './split-sentence';
 import handleComma from './handle-comma';
 
 
-// var el = document.getElementById('me');
-// el.addEventListener('click', (e) => {
-// 	console.log(e.target.innerHTML);
-// })
-
 export default {};
